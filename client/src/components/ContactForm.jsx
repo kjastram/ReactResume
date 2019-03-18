4ad78de23f4f00a47847064cabaf4a96a7aaf754
Kyle Jastram <kyle@Kyles-MacBook-Pro.local>
2019-03-18 11:24:31 -0400

    Added message sent notification and textdirection

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import { CSSTransition } from 'react-transition-group'
 import axios from 'axios'
+import Checkmark from './Checkmark'
 
 
 export default class ContactForm extends Component {
     constructor(props) {
         super(props)
-        this.handleChange = this.handleChange.bind(this)
 
         this.state = {
+            contactSent: false,
             contact: {
                 name: '',
                 email: '',
@@ -18,7 +19,7 @@ export default class ContactForm extends Component {
         }
     }
 
-    handleChange(event) {
+    handleChange = (event) => {
         const contact = { ...this.state.contact }
         contact[event.currentTarget.name] = event.currentTarget.value
         this.setState({ contact })
@@ -30,7 +31,6 @@ export default class ContactForm extends Component {
 
         try {
             const response = await axios.post("/api/contact", this.state.contact);
-            console.log(response)
         }
         catch (error) {
             if (error.response) {
@@ -44,50 +44,68 @@ export default class ContactForm extends Component {
             }
             console.log(error.config);
         }
+
+        this.setState({
+            contact: {
+                name: '',
+                email: '',
+                number: '',
+                message: ''
+            }
+        })
+        this.setState({ contactSent: true })
+        setTimeout(() => this.setState({ contactSent: false }), 2500)
     }
 
     render() {
         return (
-            <CSSTransition in={this.props.visibility} classNames="contact-form" unmountOnExit timeout={300}>
+            <React.Fragment>
+                <CSSTransition in={this.state.contactSent} classNames="checkmark-logo" mountOnEnter unmountOnExit timeout={300}>
+                    <div className="checkmark-logo">
+                        <Checkmark />
+                        <h4>Message Sent!</h4>
+                    </div>
+                </CSSTransition>
+                <CSSTransition in={this.props.visibility} classNames="contact-form" unmountOnExit timeout={300}>
+                    <form className="contact-form" onSubmit={this.handleSubmit}>
+                        <div id="close" onClick={this.props.toggleContactForm} />
 
-                <form className="contact-form" onSubmit={this.handleSubmit}>
-                    <div id="close" onClick={this.props.toggleContactForm} />
+                        <p>Email</p>
+                        <input
+                            type="text"
+                            name="email"
+                            value={this.state.contact.email}
+                            onChange={this.handleChange} />
 
-                    <p>Email</p>
-                    <input
-                        type="text"
-                        name="email"
-                        value={this.state.contact.email}
-                        onChange={this.handleChange} />
+                        <p>Name</p>
+                        <input
+                            type="text"
+                            name="name"
+                            value={this.state.contact.name}
+                            onChange={this.handleChange} />
 
-                    <p>Name</p>
-                    <input
-                        type="text"
-                        name="name"
-                        value={this.state.contact.name}
-                        onChange={this.handleChange} />
+                        <p>Phone Number</p>
+                        <input
+                            type="text"
+                            name="number"
+                            value={this.state.contact.number}
+                            onChange={this.handleChange} />
 
-                    <p>Phone Number</p>
-                    <input
-                        type="text"
-                        name="number"
-                        value={this.state.contact.number}
-                        onChange={this.handleChange} />
+                        <label id="submit">
+                            <input type="submit" id="submit-hide" />
+                            <p>Send</p>
+                        </label>
+                        <p id="message-header"> Message</p>
+                        <textarea
+                            id="message-form"
+                            type="text"
+                            name="message"
+                            value={this.state.contact.message}
+                            onChange={this.handleChange} />
+                    </form>
 
-                    <label id="submit">
-                        <input type="submit" id="submit-hide" />
-                        <p>Send</p>
-                    </label>
-                    <p id="message-header"> Message</p>
-                    <textarea
-                        id="message-form"
-                        type="text"
-                        name="message"
-                        value={this.state.contact.message}
-                        onChange={this.handleChange} />
-                </form>
-
-            </CSSTransition>
+                </CSSTransition>
+            </React.Fragment>
         )
     }
 }
