28990a83b56fd8e3f3fe52e6a2cbe76bcc67b57e
Kyle Jastram <kyle@dhcp-87-243.ram.secure.int.wireless.unca.edu>
2019-03-19 18:09:54 -0400

    change skill color scheme

--- client/src/components/MySkills.jsx
+++ client/src/components/MySkills.jsx
@@ -9,16 +9,16 @@ export default class MySkills extends Component {
                 <NavLeft>Welcome</NavLeft>
                 <div id="Skills-wrapper">
                     <div id="Skills-subgrid">
-                        <Skill color="#2E7505">Node.js</Skill>
-                        <Skill color="#9013FE">Python</Skill>
-                        <Skill color="#4A90E2">React</Skill>
-                        <Skill color="#D0021B">R</Skill>
-                        <Skill color="#AEDE7E">MongoDB</Skill>
-                        <Skill color="#50E3C2">Java</Skill>
-                        <Skill color="#F5A623">Express</Skill>
-                        <Skill color="#624311">C</Skill>
-                        <Skill color="#F5238D">HTML&CSS</Skill>
-                        <Skill color="#D8CD48">Javascript</Skill>
+                        <Skill color="#000000">Node.js</Skill>
+                        <Skill color="#000000">Python</Skill>
+                        <Skill color="#000000">React</Skill>
+                        <Skill color="#000000">R</Skill>
+                        <Skill color="#000000">MongoDB</Skill>
+                        <Skill color="#000000">Java</Skill>
+                        <Skill color="#000000">Express</Skill>
+                        <Skill color="#000000">C</Skill>
+                        <Skill color="#000000">HTML&CSS</Skill>
+                        <Skill color="#000000">Javascript</Skill>
                     </div>
                 </div>
             </React.Fragment>
--- client/src/components/Skill.jsx
+++ client/src/components/Skill.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-
+//style={{ backgroundColor: props.color }}
 const Skill = (props) => {
     return (
-        <Link className="skill" style={{ backgroundColor: props.color }} to={"/MySkills/" + props.children}>
+        <Link className="skill" to={"/MySkills/" + props.children}>
             <p>{props.children}</p>
         </Link>
     )
