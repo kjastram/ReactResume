f342e8ca92ebfe4dbf789f3c511bb6f3bdd5f920
Kyle Jastram <kyle@dhcp-86-78.ram.secure.int.wireless.unca.edu>
2019-03-21 11:57:09 -0400

    Added Github Link

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import GithubLogo from './GithubLogo';
 
 export default function Header() {
     return (
         <div id="Header">
+            <GithubLogo width="48px" />
             Kyle Jastram
         </div>
     )
