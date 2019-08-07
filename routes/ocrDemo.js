94f90b3aadced9774321c8db677e0f3f53c7fee5
Kyle Jastram <kyle@Kyles-MacBook-Pro.local>
2019-08-07 17:03:06 -0500

    ....

--- routes/ocrDemo.js
+++ routes/ocrDemo.js
@@ -36,7 +36,7 @@ ocrDemoRoute.post("/api/demo/upload", (req, res) => {
 
 
     let options = {
-        pythonPath: "/home/bitnami/anaconda3"
+        pythonPath: "/home/bitnami/anaconda3/bin/conda"
     }
 
     let pyshell = new PythonShell('./ocrDemo/Task2.py', options);
